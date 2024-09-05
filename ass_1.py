98bef555cb22c50b4e3abb5c8c9289637e5f6dbe
trilatus <104985913+trilatus@users.noreply.github.com>
2024-09-05 18:37:42 +0200

    Initial commit

---
+++ ass_1.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Total = df['MyColumn'].sum()
+#print(Total)
+#319
+#df.loc[df['a'] == 1, 'b'].sum()
+
+
+def Task_1(file):
+    
+    children = 0
+    elderly = 0
+    labor = 0
+
+    print(file)
+    for i in range(1860, 2023, 1):
+        if age < 15:
+            pass
+
+        elif age > 64:
+            pass
+
+        else:
+            pass
+    
+    
+    print(children)
+
+
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("swedish_population_by_year_and_sex_1860-2022.csv")
+    #Task_1(df)
+    index = pd.Series(range(1860, 2023, 1)) #Ska användas för att kombinera med data som filteras sedam
+    #print(df)
+    
+
