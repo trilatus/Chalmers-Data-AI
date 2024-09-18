9aac882267758c7026d54a5764503a006f486e63
trilatus <104985913+trilatus@users.noreply.github.com>
2024-09-18 17:19:40 +0200

    fixed scatter plots and color matching for rooms

--- ass_2.py
+++ ass_2.py
@@ -6,16 +6,12 @@ import matplotlib.pyplot as plt
 def FiveNumSum(data):
     return np.percentile(data, [0, 25, 50, 75, 100], method="midpoint")
 
-
 df = pd.read_csv("out.csv")
 df = df.drop("Unnamed: 0", axis=1)  #Remove dublicate id
 df["Date of sale"] = df["Date of sale"].str.slice(start=1)  #Remove added blank space from the data
 df = df[df["Date of sale"].str.contains("2022")]
-
 df["Price"] = pd.to_numeric(df["Price"].str.replace("kr", "")) #Remove kr and change the data to numeric
-
 print("Here is the five-number summary ", FiveNumSum(df["Price"]))
-#print(df)
 
 #Calculating bin size with the help of "Freedman–Diaconis rule"
 #https://how2matplotlib.com/bin-size-in-matplotlib-histogram.html
@@ -23,15 +19,23 @@ iqr = np.subtract(*np.percentile(df["Price"], [75, 25]))
 bin_width = 2 * iqr * len(df["Price"])**(-1/3)
 num_bins = int((max(df["Price"]) - min(df["Price"])) / bin_width)
 
-
-plt.hist(df["Price"], bins=num_bins, color="yellow", edgecolor = "black")
+#Plotting Histogram
+#plt.hist(df["Price"], bins=num_bins, color="yellow", edgecolor = "black")
 plt.title("Closing Price Histogram")
-
 plt.ylabel("Frequency")
 plt.xlabel("Price")
+#plt.show()
+
+df['Living area'] = pd.to_numeric(df['Living area'].str.extract(r"\('(\d+)'")[0]) #Filter so only boarea is included
+plt.scatter(df["Price"], df["Living area"])
+plt.title("Scatter plot relation price, boarea")
+plt.xlabel("Price")
+plt.ylabel("Boarea")
+#plt.show()
+
+print(df)
+df["No. rooms"] = pd.to_numeric(df["No. rooms"].str.replace("rum", "")) #Remove kr and change the data to numeric
+#z = [x for x in range(190)] # för färg skit
+plt.scatter(df["Price"], df["Living area"], c=df["No. rooms"], cmap="cividis")
+plt.colorbar().set_label("No. rooms")
 plt.show()
-
-
-#Debug
-#test = df["Date of sale"] == "31 oktober 2017"
-#print([x for x in test])
